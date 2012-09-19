8b8451d064d75e255792c31d6c80ac3b1aec0b96
moretto-nik <nicola.moretto@xpeppers.com>
2012-09-19 19:29:48 +0200

    Remove default :json

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 UhopperRest::Application.routes.draw do
-  resources :users, except: :edit
-  
-  match 'check_in' => 'users#check_in', :via => :post, :defaults => {:format => 'json'}
-  match 'tracking' => 'users#tracking', :via => :post, :defaults => {:format => 'json'}
+  match 'check_in' => 'users#check_in', :via => :post
+  match 'tracking' => 'users#tracking', :via => :post
   match 'check_out' => 'users#check_out', :via => :delete
 end
