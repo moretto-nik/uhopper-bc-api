308c8ccc7690a318fea6d8f1f92b1775cee8b887
moretto-nik <nicola.moretto@xpeppers.com>
2012-09-20 20:24:56 +0200

    modify delete route without parameters

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 UhopperRest::Application.routes.draw do
   match 'check_in' => 'users#check_in', :via => :post
   match 'tracking' => 'users#tracking', :via => :post
-  match 'check_out' => 'users#check_out', :via => :delete
+  match 'check_out/:id_cart' => 'users#check_out', :via => :delete
 end
