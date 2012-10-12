880655c5b0ed43eba5ffe7dcc5a04a9f64995092
moretto-nik <nicola.moretto@xpeppers.com>
2012-10-12 15:19:28 +0200

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require "bundler/capistrano"
 require "rvm/capistrano"
 
-set :application, "beancounter.local"
+set :application, "beancounter-rest"
 set :deploy_to, "/var/www/#{application}"
 
 set :scm, :git
@@ -9,14 +9,14 @@ set :scm_user, "moretto-nik"
 set :repository, "git@github.com:moretto-nik/uhopper-bc-api.git"
 set :branch, "master"
 
-set :user, 'vagrant'
-set :scm_passphrase, "vagrant"
+set :user, 'beancounter'
+set :scm_passphrase, "beancounterpwd7"
 ssh_options[:forward_agent] = true 
 default_run_options[:pty] = true 
 
-role :web, "beancounter.local"                         # Your HTTP server, Apache/etc
-role :app, "beancounter.local"                         # This may be the same as your `Web` server
-role :db, "beancounter.local", :primary => true        # This is where Rails migrations will run
+role :web, "beancounter-rest"                      # Your HTTP server, Apache/etc
+role :app, "beancounter-rest"                         # This may be the same as your `Web` server
+role :db, "beancounter-rest", :primary => true        # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
